4579f81684814ab88a99dc397b30f7eaa97f61b2
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-05-26 14:17:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+//variables
+form = document.querySelector('#form');
+email = document.querySelector(#email);
+
+//eventlistner
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+  })
+
+//functions
+function checkInputs(){
+    const emailValue = email.value.trim();
+
+    if(emailValue === ""){
+        setErrorFor(email, "Email cannot be empty")
+      } else if(isEmail(emailValue)) {
+        setErrorFor(email, 'Enter valid email addres')
+      }
+}
+
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+  
+    //add error message inside small
+    small.innerText = message;
+  
+    //add error class
+    formControl.className ='form-control error'
+  }
+
+function isEmail(email){
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+  }
